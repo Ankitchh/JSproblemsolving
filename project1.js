78d91590643e5b562bb4ef321fd01075d9df41f9
Ankitchh <ankichh1234@gmail.com>
2023-10-18 20:14:39 +0530

    added some new line of code

--- project1.js
+++ project1.js
@@ -3,10 +3,55 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-function circleMouseFollower(){
-    window.addEventListener("mousemove",function(details){
-        this.document.querySelector("#minicircle").style.transform = `translate(${details.clientX}px,${details.clientY}px)`;
+function firstPageAnimation(){
+    var tl = gsap.timeline();
+
+
+    tl.from("nav",{
+        y: '-10%',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    .to(".boundelem",{
+        y: 0,
+        duration: 2,
+        delay:-1,
+        ease: Expo.easeInOut,
+        stagger:.2
+    })
+    .from("#herofooter",{
+        y: '-10%',
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+    })
+}
+
+function circleSquizer(){
+    var xScale = 1;
+    var yScale = 1;
+
+    var xpre = 0;
+    var ypre = 0;
+    window.addEventListener("mousemove", function(dets){
+
+        xScale = gsap.utils.clamp(.8,1.2, dets.clientX - xpre);
+        yScale = gsap.utils.clamp(.8,1.2, dets.clienty - ypre);
+
+        xpre = dets.clientX;
+        ypre = dets.clientY;
+        
+        circleMouseFollower(xScale,yScale);
+    })
+}
+function circleMouseFollower(xScale,yScale){
+    window.addEventListener("mousemove",function(dets){
+        this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${details.clientY}px) scale(${xScale}, ${yScale})`;
     })
 }
 
 circleMouseFollower();
+circleSquizer();
+firstPageAnimation();
