36fc2299f310fc40d9155eeb5ca283e3617f7c78
Ankitchh <ankichh1234@gmail.com>
2023-12-16 21:09:20 +0530

    Final complete project

--- project1.js
+++ project1.js
@@ -28,30 +28,65 @@ function firstPageAnimation(){
         ease: Expo.easeInOut
     })
 }
-
+var timeout;
 function circleSquizer(){
-    var xScale = 1;
-    var yScale = 1;
+    var xscale = 1;
+    var yscale = 1;
 
     var xpre = 0;
     var ypre = 0;
     window.addEventListener("mousemove", function(dets){
+        clearTimeout(timeout);
 
-        xScale = gsap.utils.clamp(.8,1.2, dets.clientX - xpre);
-        yScale = gsap.utils.clamp(.8,1.2, dets.clienty - ypre);
+        xscale = gsap.utils.clamp(.8,1.2, dets.clientX - xpre);
+        yscale = gsap.utils.clamp(.8,1.2, dets.clienty - ypre);
 
         xpre = dets.clientX;
         ypre = dets.clientY;
         
-        circleMouseFollower(xScale,yScale);
-    })
+        circleMouseFollower(xscale, yscale);
+        timeout = setTimeout(function(){
+            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`;
+        },100);
+    });
 }
-function circleMouseFollower(xScale,yScale){
+circleSquizer();
+function circleMouseFollower(xscale, yscale){
     window.addEventListener("mousemove",function(dets){
-        this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px,${details.clientY}px) scale(${xScale}, ${yScale})`;
+       document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
     })
 }
 
 circleMouseFollower();
-circleSquizer();
+
 firstPageAnimation();
+
+document.querySelectorAll(".elem").forEach(function(elem){
+
+    var rotate = 0;
+    var r_diff = 0;
+
+    elem.addEventListener("mouseleave", function(){
+        gsap.to(elem.querySelector("img"),{
+            opacity: 0,
+            ease: Power3,
+        });
+    });
+
+
+   
+
+
+    elem.addEventListener("mousemove", function(details){
+        var diff = details.clientY - elem.getBoundingClientRect().top;
+        r_diff = details.clientX - rotate;
+        rotate = details.clientX;
+        gsap.to(elem.querySelector("img"),{
+            opacity: 1,
+            ease: Power3,
+            top: diff,
+            left: details.clientX,
+            rotate: gsap.utils.clamp(-20,20,r_diff)
+        });
+    });
+});
